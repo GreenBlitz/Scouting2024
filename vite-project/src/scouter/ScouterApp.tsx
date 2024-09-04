1feab84607c2e9c7b95e37aa6145ba8eef815183
greenblitz <greenblitz@gmail.com>
2024-09-04 20:20:39 +0300

    Yoni - working, going to clean

--- vite-project/src/scouter/ScouterApp.tsx
+++ vite-project/src/scouter/ScouterApp.tsx
@@ -1,51 +1,50 @@
 import { useState } from "react";
 import ScouterQuery from "./ScouterQuery";
-import ScouterTab from "./ScouterTab";
-import QRCodeGenerator from "../functions/QRCode-Generator";
+import QRCodeGenerator from "../components/QRCode-Generator";
 
 function ScouterApp() {
+  const [formData, setFormData] = useState<Record<string, string> | undefined>(
+    undefined
+  );
 
-    function handleSubmit(
-        event: React.FormEvent<HTMLFormElement>, 
-        setFormData: React.Dispatch<React.SetStateAction<Record<string, string> | undefined>>, 
-    ){
-        const formData = new FormData(event.currentTarget);
-        event.preventDefault();
-        let formValues: Record<string, string> = {};
-        for (let [key, value] of formData.entries()) {
-            formValues[key] = value.toString();
-        }
-        setFormData(formValues);
-        localStorage.clear();
+  function handleSubmit(
+    event: React.FormEvent<HTMLFormElement>
+  ) {
+    const formData = new FormData(event.currentTarget);
+    event.preventDefault();
+    let formValues: Record<string, string> = {};
+    for (let [key, value] of formData.entries()) {
+      formValues[key] = value.toString();
     }
+    setFormData(formValues);
+    localStorage.clear(); //TODO: wtf is this
+  }
 
-    function handleReset() {
-        localStorage.clear();
-        window.location.reload();
-    }
+  function handleReset() {
+    localStorage.clear();
+    window.location.reload();
+  }
 
-    const [formData, setFormData] = useState<Record<string,string> | undefined>(undefined);
+  if (formData) {
+    return <QRCodeGenerator text={JSON.stringify(formData)} />;
+  }
 
-    if (formData)
-        return <QRCodeGenerator text={JSON.stringify(formData)} />
-
-
-    return (
-    <>
-    <form onSubmit={(event) => handleSubmit(event, setFormData)} onReset={handleReset}>
-        <ScouterTab name="Tests">
-            <ScouterQuery queryType="text" name="Test 1 " />
-            <ScouterQuery queryType="checkbox" name="Test 2 " />
-            <ScouterQuery queryType="counter" name="Test 3 " />
-            <ScouterQuery queryType="number" name="Test 4 " />
-            <ScouterQuery queryType="list" name="Test 5 ">{["1","2","3"]}</ScouterQuery>
-            <ScouterQuery queryType="radio" name="Test 6 ">{["a","b","c","d"]}</ScouterQuery>
-        </ScouterTab>
-        <button type="submit">Submit</button>
-        <button type="reset">Reset</button>
+  return (
+    <form
+      onSubmit={handleSubmit}
+      onReset={handleReset}
+    >
+        <h1 className="scouter-tab">Tests</h1>
+        <ScouterQuery queryType="text" name="Test 1 " />
+        <ScouterQuery queryType="checkbox" name="Test 2 " />
+        <ScouterQuery queryType="counter" name="Test 3 " />
+        <ScouterQuery queryType="number" name="Test 4 " />
+        <ScouterQuery queryType="list" name="Test 5 " listOptions={["1", "2", "3"]} />
+        <ScouterQuery queryType="radio" name="Test 6 " listOptions={["a", "b", "c", "d"]} />
+      <button type="submit">Submit</button>
+      <button type="reset">Reset</button>
     </form>
-    </>
-    );
+  );
 }
 
 export default ScouterApp;
--- vite-project/src/scouter/ScouterQuery.tsx
+++ vite-project/src/scouter/ScouterQuery.tsx
@@ -1,81 +1,134 @@
 import { useState } from "react";
 
-interface Props {
-    name: string;
-    queryType: "text" | "counter" | "checkbox" | "number" | "list" | "radio";
-    required?: boolean | undefined;
-    children?: string[];
+interface ScouterQueryProps {
+  name: string;
+  queryType: "text" | "counter" | "checkbox" | "number" | "list" | "radio";
+  required?: boolean | undefined;
+  listOptions?: string[];
 }
 
-function ScouterQuery({name, queryType, required, children} : Props) {
-
-    
-    
-    function renderInput() {
-        const initialValue = localStorage.getItem(name) || "";
-        if (queryType === "counter") {
-            return <CounterQuery name={name} defaultValue={initialValue}/>
-        }
-    
-        if (queryType === "list") {
-            return (
-            <select name={name} id={name} required={required} defaultValue={initialValue} onChange={(event) => localStorage.setItem(name, event.target.value)}>
-                {children?.map((item) => <option value={item} key={item}>{item}</option>)}
-            </select>
-            )
-        }
-        
-        if (queryType === "radio") {
-            return (
-                children?.map((item) => 
-                <>
-                {
-                    item === initialValue 
-                    ? <input type="radio" id={item} name={name} value={item} required={required} onChange={() => localStorage.setItem(name, item)} defaultChecked/>
-                    : <input type="radio" id={item} name={name} value={item} required={required} onChange={() => localStorage.setItem(name, item)}/>
-                }
-                <label htmlFor={item}>{item}</label>
-                </>
-                )
-            )
-        }
-        if (queryType === "checkbox") {
-            function updateCheckbox() {
-                const newValue = localStorage.getItem(name) === "true" ? "false" : "true";
-                localStorage.setItem(name, newValue);
-            }
-            return <input type={queryType} id={name} name={name} required={required} onChange={updateCheckbox} defaultChecked={initialValue === "true"}/>
-        }
-    
-        return <input type={queryType} id={name} name={name} required={required} defaultValue={initialValue} onChange={(event) => localStorage.setItem(name, event.target.value)}/>
+const ScouterQuery: React.FC<ScouterQueryProps> = ({
+  name,
+  queryType,
+  required,
+  listOptions: list
+}) => {
+  function renderInput() {
+    const initialValue = localStorage.getItem(name) || "";
+    if (queryType === "counter") {
+      return <CounterQuery name={name} defaultValue={initialValue} />;
     }
 
-    return (
-    <div className="scouter-query">
-        <h2>{name}</h2>
-        {renderInput()}
-    </div>
-    );
-}
-
-function CounterQuery({name, defaultValue} : {name:string, defaultValue: string}) {
-
-    const startingNumber = defaultValue === "" ? 0 : parseInt(defaultValue);
-    const [count, setCountState] = useState(startingNumber);
-
-    function setCount(newCount: number) {
-        localStorage.setItem(name,newCount + "");
-        setCountState(newCount)
+    if (queryType === "list") {
+      return (
+        <select
+          name={name}
+          id={name}
+          required={required}
+          defaultValue={initialValue}
+          onChange={(event) => localStorage.setItem(name, event.target.value)}
+        >
+          {list?.map((item: string) => (
+            <option value={item} key={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+      );
     }
 
-    return (
+    if (queryType === "radio") {
+      return list?.map((item: string) => (
         <>
-        <button type="button" onClick={() => setCount(Math.max(count - 1,0))}>-</button>
-            <h3>{count}</h3>
-            <input type="hidden" id={name} name={name} value={count}/>
-        <button type="button" onClick={() => setCount(count + 1)}>+</button>
+          {item === initialValue ? (
+            <input
+              type="radio"
+              id={item}
+              name={name}
+              value={item}
+              required={required}
+              onChange={() => localStorage.setItem(name, item)}
+              defaultChecked
+            />
+          ) : (
+            <input
+              type="radio"
+              id={item}
+              name={name}
+              value={item}
+              required={required}
+              onChange={() => localStorage.setItem(name, item)}
+            />
+          )}
+          <label htmlFor={item}>{item}</label>
         </>
-    )
+      ));
+    }
+    if (queryType === "checkbox") {
+      function updateCheckbox() {
+        const newValue =
+          localStorage.getItem(name) === "true" ? "false" : "true";
+        localStorage.setItem(name, newValue);
+      }
+      return (
+        <input
+          type={queryType}
+          id={name}
+          name={name}
+          required={required}
+          onChange={updateCheckbox}
+          defaultChecked={initialValue === "true"}
+        />
+      );
+    }
+
+    return (
+      <input
+        type={queryType}
+        id={name}
+        name={name}
+        required={required}
+        defaultValue={initialValue}
+        onChange={(event) => localStorage.setItem(name, event.target.value)}
+      />
+    );
+  }
+
+  return (
+    <div className="scouter-query">
+      <h2>{name}</h2>
+      {renderInput()}
+    </div>
+  );
+};
+
+function CounterQuery({
+  name,
+  defaultValue,
+}: {
+  name: string;
+  defaultValue: string;
+}) {
+  const startingNumber = defaultValue === "" ? 0 : parseInt(defaultValue);
+  const [count, setCountState] = useState(startingNumber);
+
+  function setCount(newCount: number) {
+    localStorage.setItem(name, newCount + "");
+    setCountState(newCount);
+  }
+
+  return (
+    <>
+      <button type="button" onClick={() => setCount(Math.max(count - 1, 0))}>
+        -
+      </button>
+      <h3>{count}</h3>
+      <input type="hidden" id={name} name={name} value={count} />
+      <button type="button" onClick={() => setCount(count + 1)}>
+        +
+      </button>
+    </>
+  );
 }
 
 export default ScouterQuery;
