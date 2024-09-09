a6a220dbf82e633efb8a68199545a0e7c4562b0a
greenblitz <greenblitz@gmail.com>
2024-09-09 19:07:35 +0300

    Yoni - deleted Map Query from this branch

--- vite-project/src/scouter/ScouterApp.tsx
+++ vite-project/src/scouter/ScouterApp.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import ScouterQuery from "./ScouterQuery";
 import QRCodeGenerator from "../components/QRCode-Generator";
-import MapQuery from "./querytypes/mapquery/MapQuery";
-import MapButton from "./querytypes/mapquery/MapButton";
 
 function ScouterApp() {
   const [formData, setFormData] = useState<Record<string, string> | undefined>(
@@ -41,13 +39,6 @@ function ScouterApp() {
         name="Test 6 "
         list={["a", "b", "c", "d"]}
       />
-      <MapQuery
-        width={500}
-        height={500}
-        imagePath="./src/assets/Crescendo Map.png"
-      >
-        <MapButton name="sus" />
-      </MapQuery>
       <button type="submit">Submit</button>
       <button type="reset">Reset</button>
     </form>
