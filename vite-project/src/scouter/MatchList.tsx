d91cf2b64d364e4acad40f2a7e6757e944781b38
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-09-17 19:31:06 +0300

    Yoni - cleaned

--- vite-project/src/scouter/MatchList.tsx
+++ vite-project/src/scouter/MatchList.tsx
@@ -3,29 +3,32 @@ import Collapsible from "react-collapsible";
 import React, { useState } from "react";
 import QRCodeGenerator from "../components/QRCode-Generator";
 
+const matchName = "Qual";
+const matchesTab = "Matches/";
+
 const MatchList: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
   const [matches, setMatches] = useState<Record<string, string>[]>(
     Object.keys(localStorage)
-      .filter((match) => match.startsWith("Matches/"))
+      .filter((match) => match.startsWith(matchesTab))
       .map((matchName) => JSON.parse(localStorage.getItem(matchName) || "{}"))
   );
 
   const latestMatch = location.state;
   location.state = {};
 
-  if (latestMatch?.["Qual"]) {
+  if (latestMatch?.[matchName]) {
     matches.push(latestMatch);
     localStorage.setItem(
-      `Matches/${latestMatch["Qual"]}`,
+      matchesTab + latestMatch[matchName],
       JSON.stringify(latestMatch)
     );
   }
 
   function removeMatch(qualNumber: string, index: number) {
-    localStorage.removeItem(`Matches/${qualNumber}`);
+    localStorage.removeItem(matchesTab + qualNumber);
     const filtered = [...matches];
     filtered.splice(index, 1);
     setMatches(filtered);
@@ -35,14 +38,14 @@ const MatchList: React.FC = () => {
     <div className="match-list">
       {matches.map((match, index) => (
         <Collapsible
-          trigger={`Qual ${match["Qual"]}`}
+          trigger={`${matchName} ${match[matchName]}`}
           triggerStyle={{ borderStyle: "solid" }}
         >
           <QRCodeGenerator text={JSON.stringify(match)} />
           <br />
           <button
             type="button"
-            onClick={() => removeMatch(match?.["Qual"], index)}
+            onClick={() => removeMatch(match?.[matchName], index)}
           >
             Delete
           </button>
