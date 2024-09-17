7e0dfb2c53f150aec82fdd54550198d5a2563571
itamaroryan <itamar.oryan@gmail.com>
2024-09-17 18:13:39 +0300

    Yoni - npam took my pc :(

--- vite-project/src/scouter/MatchList.tsx
+++ vite-project/src/scouter/MatchList.tsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import Collapsible from "react-collapsible";
-import { useState } from "react";
+import React, { useState } from "react";
 import QRCodeGenerator from "../components/QRCode-Generator";
 
 interface MatchListProps {}
@@ -8,8 +8,14 @@ interface MatchListProps {}
 const MatchList: React.FC<MatchListProps> = ({}) => {
   const location = useLocation();
   const navigate = useNavigate();
-  const [matches] = useState<Record<string, string>[]>([]);
+
+  const [matches, setMatches] = useState<Record<string, string>[]>(
+    Object.keys(localStorage)
+      .filter((match) => match.startsWith("Matches/"))
+      .map((matchName) => JSON.parse(localStorage.getItem(matchName) || "{}"))
+  );
   const latestMatch = location.state;
+  location.state = {};
   if (latestMatch?.["Qual"]) {
     matches.push(latestMatch);
     localStorage.setItem(
@@ -18,11 +24,26 @@ const MatchList: React.FC<MatchListProps> = ({}) => {
     );
   }
 
+  function removeMatch(qualNumber: string, index: number) {
+    localStorage.removeItem(`Matches/${qualNumber}`);
+    const filtered = [...matches].splice(index, 1);
+    console.log(index);
+    console.log(filtered);
+    setMatches(filtered);
+  }
+
   return (
     <>
-      {matches.map((match) => (
+      {matches.map((match, index) => (
         <Collapsible trigger={match["Qual"]}>
           <QRCodeGenerator text={JSON.stringify(match)} />
+          <br />
+          <button
+            type="button"
+            onClick={() => removeMatch(match?.["Qual"], index)}
+          >
+            Delete
+          </button>
         </Collapsible>
       ))}
       <button type="button" onClick={() => navigate("/")}>
