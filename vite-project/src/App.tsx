34660d267bdf15548eea6abd25bde71b1e5cc4ac
Nitay4 <nitay@rotbart.co.il>
2024-09-22 15:32:12 +0300

    Nitay - fixed a few functions and did css

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,14 +4,14 @@ import { RadarComponent } from "./strategypage/RadarChart";
 import { RadarInput } from "./strategypage/RadarChart";
 
 function App() {
-  const input2: RadarInput = { name: "0", max: 100, value: 50 };
-  const input3: RadarInput = { name: "1", max: 100, value: 60 };
-  const input4: RadarInput = { name: "2 ", max: 100, value: 55 };
-  const input5: RadarInput = { name: "3", max: 100, value: 20 };
-  const input6: RadarInput = { name: "4", max: 100, value: 70 };
-  const input7: RadarInput = { name: "5", max: 100, value: 40 };
-  const input8: RadarInput = { name: "6", max: 100, value: 65 };
-  const input9: RadarInput = { name: "7", max: 100, value: 80 };
+  const input2: RadarInput = { name: "amountOfApples", max: 100, value: 15.5 };
+  const input3: RadarInput = { name: "pain", max: 100, value: 99 };
+  const input4: RadarInput = { name: "RP ", max: 100, value: 55 };
+  const input5: RadarInput = { name: "defend time", max: 100, value: 50 };
+  const input6: RadarInput = { name: "climb spots", max: 100, value: 70 };
+  const input7: RadarInput = { name: "violatin", max: 100, value: 40 };
+  const input8: RadarInput = { name: "bullshit 1", max: 100, value: 65 };
+  const input9: RadarInput = { name: "bullshit 2", max: 100, value: 10 };
 
   const inputsArr: RadarInput[] = [
     input2,
