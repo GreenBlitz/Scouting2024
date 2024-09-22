a25c06a3408d9febb77642b7306924f2fe55df6c
Nitay4 <nitay@rotbart.co.il>
2024-09-22 20:35:10 +0300

    Nitay - working on cr

--- vite-project/src/strategypage/RadarChart.tsx
+++ vite-project/src/strategypage/RadarChart.tsx
@@ -31,29 +31,30 @@ export const RadarComponent: React.FC<RadarComponentProps> = ({ inputs }) => {
   ): void {
     const context = canvas.getContext("2d");
     const canvasRadius: number = canvas.width / 2;
-    if (context) {
-      context.beginPath();
-
-      for (let i = 0; i < inputs.length + 1; i++) {
-        const currentX: number =
-          polygonRadius * Math.sin((i * Math.PI * 2) / inputs.length) +
-          canvasRadius;
-        const currentY: number =
-          polygonRadius * Math.cos((i * Math.PI * 2) / inputs.length) +
-          canvasRadius;
-
-        context.lineTo(currentX, currentY);
-        context.lineTo(canvasRadius, canvasRadius);
-        context.lineTo(currentX, currentY);
-
-        const currentPoint: point = { x: currentX, y: currentY };
-
-        polygonPoints[i] = currentPoint;
-      }
-      context.closePath();
-      context.strokeStyle = stokeColor;
-      context.stroke();
+    if (!context) {
+      return;
     }
+    context.beginPath();
+
+    for (let i = 0; i < inputs.length + 1; i++) {
+      const currentX: number =
+        polygonRadius * Math.sin((i * Math.PI * 2) / inputs.length) +
+        canvasRadius;
+      const currentY: number =
+        polygonRadius * Math.cos((i * Math.PI * 2) / inputs.length) +
+        canvasRadius;
+
+      context.lineTo(currentX, currentY);
+      context.lineTo(canvasRadius, canvasRadius);
+      context.lineTo(currentX, currentY);
+
+      const currentPoint: point = { x: currentX, y: currentY };
+
+      polygonPoints[i] = currentPoint;
+    }
+    context.closePath();
+    context.strokeStyle = stokeColor;
+    context.stroke();
   }
 
   //function that draws the inner regular polygos
@@ -65,27 +66,28 @@ export const RadarComponent: React.FC<RadarComponentProps> = ({ inputs }) => {
   ): void {
     const context = canvas.getContext("2d");
     const canvasRadius: number = canvas.width / 2;
-    if (context) {
-      context.beginPath();
-
-      for (let i = 0; i < inputs.length + 1; i++) {
-        const currentX: number =
-          polygonRadius * Math.sin((i * Math.PI * 2) / inputs.length) +
-          canvasRadius;
-        const currentY: number =
-          polygonRadius * Math.cos((i * Math.PI * 2) / inputs.length) +
-          canvasRadius;
-
-        context.lineTo(currentX, currentY);
-
-        const currentPoint: point = { x: currentX, y: currentY };
-
-        polygonPoints[i] = currentPoint;
-      }
-      context.closePath();
-      context.strokeStyle = stokeColor;
-      context.stroke();
+    if (!context) {
+      return;
     }
+    context.beginPath();
+
+    for (let i = 0; i < inputs.length + 1; i++) {
+      const currentX: number =
+        polygonRadius * Math.sin((i * Math.PI * 2) / inputs.length) +
+        canvasRadius;
+      const currentY: number =
+        polygonRadius * Math.cos((i * Math.PI * 2) / inputs.length) +
+        canvasRadius;
+
+      context.lineTo(currentX, currentY);
+
+      const currentPoint: point = { x: currentX, y: currentY };
+
+      polygonPoints[i] = currentPoint;
+    }
+    context.closePath();
+    context.strokeStyle = stokeColor;
+    context.stroke();
   }
 
   //function that puts lables in a regular shape placement
@@ -102,112 +104,157 @@ export const RadarComponent: React.FC<RadarComponentProps> = ({ inputs }) => {
         polygonRadius * Math.cos((i * Math.PI * 2) / inputs.length) +
         canvasRadius;
 
-      if (context) {
-        context.fillText(inputs[i].name, currentX, currentY);
+      if (!context) {
+        return;
       }
+      context.fillText(inputs[i].name, currentX, currentY);
     }
   }
 
+  //functions that findes stat points
+  function findeStatPoints(
+    canvas: HTMLCanvasElement,
+    polygonPoints: point[]
+  ): void {
+    for (let i = 0; i < inputs.length; i++) {
+      const canvasRadius: number = canvas.width / 2;
+
+      let k = inputs[i].value;
+      const l = inputs[i].max - inputs[i].value;
+
+      const currentX: number =
+        (canvasRadius * l + polygonPoints[i].x * k) / inputs[i].max;
+      const currentY: number =
+        (canvasRadius * l + polygonPoints[i].y * k) / inputs[i].max;
+
+      const currentPoint: point = { x: currentX, y: currentY };
+      statPoints[i] = currentPoint;
+    }
+  }
+
+  // function that draws inner shape and fills it in
+  function drawStatPolygon(
+    context: CanvasRenderingContext2D,
+    polygonPoints: point[],
+    fillStyle: string
+  ): void {
+    context.beginPath();
+    context.fillStyle = fillStyle;
+
+    for (let i = 0; i < inputs.length; i++) {
+      context.lineTo(statPoints[i].x, statPoints[i].y);
+    }
+
+    context.lineTo(statPoints[0].x, statPoints[0].y);
+
+    context.closePath();
+  }
+
+  //function that places the 10-100 digit
+  function placeStatDigits(
+    context: CanvasRenderingContext2D,
+    fillStyle: string,
+    strokeStyle: string
+  ): void {
+    const canvasRadius: number = context.canvas.width / 2;
+    const polygonRadius: number = canvasRadius * 0.75;
+
+    const digitPoint: point = {
+      x: polygonRadius + polygonRadius / 3.35,
+      y: polygonRadius + polygonRadius / 2.3,
+    };
+
+    context.strokeStyle = strokeStyle;
+    context.fillStyle = fillStyle;
+
+    for (let i = 1; i <= 10; i++) {
+      context.font = "15px Arial";
+      context.strokeText(String(i * 10), digitPoint.x, digitPoint.y);
+
+      context.fill();
+
+      context.font = "15px Arial";
+      context.fillText(String(i * 10), digitPoint.x, digitPoint.y);
+
+      context.fill();
+
+      digitPoint.y = digitPoint.y + polygonRadius / 10;
+      context.moveTo(digitPoint.x, digitPoint.y);
+    }
+  }
+
+  // draws the commponent
   useEffect(() => {
     const canvas = canvasRef.current;
-
-    if (canvas) {
-      const context = canvas.getContext("2d");
-      if (context) {
-        const canvasRadius: number = canvas.width / 2;
-        const polygonRadius: number = canvasRadius * 0.75;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-
-        context.moveTo(canvasRadius, canvasRadius);
-
-        context.beginPath();
-        context.lineWidth = 1.5;
-
-        for (var i = 1; i < 10; i++) {
-          drawInnerPolygon((polygonRadius * i) / 10, canvas, "gray");
-          context.moveTo(canvasRadius, canvasRadius);
-        }
-
-        context.closePath();
-
-        context.beginPath();
-
-        context.lineWidth = 3;
-
-        drawOuterPolygon(polygonRadius, canvas, polygonPoints, "white");
-
-        context.closePath();
-
-        context.beginPath();
-        context.font = "15px Arial";
-
-        //puts lablse on the corners
-        drawLables(polygonRadius * 1.21, canvas);
-
-        context.closePath();
-        context.lineWidth = 3;
-        context.fill();
-        context.stroke();
-
-        context.beginPath();
-
-        //findes stat points
-        for (let i = 0; i < inputs.length; i++) {
-          var k = inputs[i].value;
-          const l = inputs[i].max - inputs[i].value;
-
-          const currentX: number =
-            (canvasRadius * l + polygonPoints[i].x * k) / inputs[i].max;
-          const currentY: number =
-            (canvasRadius * l + polygonPoints[i].y * k) / inputs[i].max;
-
-          const currentPoint: point = { x: currentX, y: currentY };
-          statPoints[i] = currentPoint;
-        }
-
-        context.closePath();
-
-        context.beginPath();
-        context.fillStyle = "rgb(96, 190, 235, 0.5)";
-
-        // draws inner shape and fills it in
-        for (let i = 0; i < inputs.length; i++) {
-          context.lineTo(statPoints[i].x, statPoints[i].y);
-        }
-
-        context.lineTo(statPoints[0].x, statPoints[0].y);
-
-        context.closePath();
-        context.lineWidth = 3;
-        context.stroke();
-        context.fill();
-
-        context.beginPath();
-        //places the 10-100 digit
-        const digitPoint: point = {
-          x: polygonRadius + polygonRadius / 3.35,
-          y: polygonRadius + polygonRadius / 2.3,
-        };
-
-        context.strokeStyle = "black";
-        context.fillStyle = "white";
-
-        for (let i = 1; i <= 10; i++) {
-          context.font = "15px Arial";
-          context.strokeText(String(i * 10), digitPoint.x, digitPoint.y);
-
-          context.fill();
-
-          context.font = "15px Arial";
-          context.fillText(String(i * 10), digitPoint.x, digitPoint.y);
-
-          context.fill();
-
-          digitPoint.y = digitPoint.y + polygonRadius / 10;
-          context.moveTo(digitPoint.x, digitPoint.y);
-        }
-      }
+    if (!canvas) {
+      return;
     }
+    const context = canvasRef.current.getContext("2d");
+    if (!context) {
+      return;
+    }
+
+    const canvasRadius: number = canvas.width / 2;
+    const polygonRadius: number = canvasRadius * 0.75;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // draws the inner polygons
+
+    context.moveTo(canvasRadius, canvasRadius);
+
+    context.beginPath();
+    context.lineWidth = 1.5;
+
+    for (var i = 1; i < 10; i++) {
+      drawInnerPolygon((polygonRadius * i) / 10, canvas, "gray");
+      context.moveTo(canvasRadius, canvasRadius);
+    }
+
+    context.closePath();
+
+    //draws the outter polygon
+
+    context.beginPath();
+
+    context.lineWidth = 3;
+
+    drawOuterPolygon(polygonRadius, canvas, polygonPoints, "white");
+
+    context.closePath();
+
+    //puts lablse on the corners
+
+    context.beginPath();
+    context.font = "15px Arial";
+
+    drawLables(polygonRadius * 1.21, canvas);
+
+    context.closePath();
+    context.lineWidth = 3;
+    context.fill();
+    context.stroke();
+
+    //findes stat points
+
+    context.beginPath();
+
+    findeStatPoints(canvas, polygonPoints);
+
+    context.closePath();
+
+    // draws inner shape and fills it in
+
+    drawStatPolygon(context, polygonPoints, "rgb(96, 190, 235, 0.5)");
+
+    context.lineWidth = 3;
+    context.stroke();
+    context.fill();
+
+    //places the 10-100 digit
+
+    context.beginPath();
+
+    placeStatDigits(context, "white", "black");
   });
 
   return (
@@ -216,5 +263,4 @@ export const RadarComponent: React.FC<RadarComponentProps> = ({ inputs }) => {
     </>
   );
 };
-
 export default RadarComponent;
