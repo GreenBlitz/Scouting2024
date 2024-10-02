83f651005dd0613b3b0142f3d343613ff79fb086
Yoni Kiriaty <YoniKiriaty@gmail.com>
2024-10-02 15:11:47 +0300

    Yoni - stuff

--- src/strategy/StrategyApp.tsx
+++ src/strategy/StrategyApp.tsx
@@ -1,21 +1,38 @@
 import RadarComponent, { RadarInput } from "./RadarChart";
+import TableChart from "./TableChart";
 
 interface StrategyAppProps {}
 const StrategyApp: React.FC<StrategyAppProps> = () => {
-  const inputsArr: RadarInput[] = [
-    { name: "amountOfApples", max: 100, value: 15.5 },
-    { name: "pain", max: 100, value: 99 },
-    { name: "RP ", max: 100, value: 55 },
-    { name: "defend time", max: 100, value: 50 },
-    { name: "climb spots", max: 100, value: 70 },
-    { name: "violatin", max: 100, value: 40 },
-    { name: "bullshit 1", max: 100, value: 65 },
-    { name: "bullshit 2", max: 100, value: 10 },
-  ];
-
   return (
     <>
-      <RadarComponent inputs={inputsArr} size={800} substeps={10} />
+      <RadarComponent
+        inputs={[
+          { name: "amountOfApples", max: 100, value: 15.5 },
+          { name: "pain", max: 100, value: 99 },
+          { name: "RP ", max: 100, value: 55 },
+          { name: "defend time", max: 100, value: 50 },
+          { name: "climb spots", max: 100, value: 70 },
+          { name: "violatin", max: 100, value: 40 },
+          { name: "bullshit 1", max: 100, value: 65 },
+          { name: "bullshit 2", max: 100, value: 10 },
+        ]}
+        size={800}
+        substeps={10}
+      />
+      <TableChart
+        matches={[
+          { Qual: "1", Team: "4590", Speaker: "6", Amp: "3" },
+          { Qual: "2", Team: "1690", Speaker: "2", Amp: "4" },
+          { Qual: "3", Team: "4590", Speaker: "6", Amp: "5" },
+        ]}
+        calculations={{
+          Score: (match) =>
+            parseInt(match["Speaker"]) * 2.5 + parseInt(match["Amp"]) + "",
+        }}
+        idName={"Qual"}
+        height={500}
+        widthOfItem={130}
+      ></TableChart>
     </>
   );
 };
