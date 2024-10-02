7dcb2b1922466c9c34000a544ba284cfc3e2329e
Yoni Kiriaty <YoniKiriaty@gmail.com>
2024-10-02 14:27:58 +0300

    Yoni -clenaed

--- src/strategy/StrategyApp.tsx
+++ src/strategy/StrategyApp.tsx
@@ -3,18 +3,7 @@ import TableChart from "./TableChart";
 interface StrategyAppProps {}
 const StrategyApp: React.FC<StrategyAppProps> = () => {
   return (
-    <TableChart
-      matches={[
-        { Test1: "Jimmy", Test3: "Mr P", Jimmy: "Shimmy" },
-        { Test1: "sigma", Test2: "sigmaless", Jimmy: "Johannesburg" },
-      ]}
-      calculations={{
-        skibidi: (match) => match["Test1"] + " " + match["Jimmy"],
-      }}
-      idName="Test1"
-      widthOfItem={130}
-      height={400}
-    ></TableChart>
+    
   );
 };
 
