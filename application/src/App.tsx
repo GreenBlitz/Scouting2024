0f02b5e3204c71bb0654ad32032af59ac132a4c4
Yoni Kiriaty <YoniKiriaty@gmail.com>
2024-10-20 01:32:34 +0300

    Yoni - canvas test

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -11,8 +11,14 @@ import TeamTab from "./strategy/TeamTab";
 function getHiddenImage(path: string) {
   return (
     <div
-      style={{ backgroundImage: 'url("' + path + '")', width: 100, height: 100 }}
-    />
+      style={{
+        backgroundImage: 'url("' + path + '")',
+        width: 100,
+        height: 100,
+      }}
+    >
+      <canvas width={100} height={100}></canvas>
+    </div>
   );
 }
 
